f4e51443241b1825fdddfbf56000b2b19df468d8
dwanyds <95312677+dwanyds@users.noreply.github.com>
2022-07-12 23:25:56 +0530

    Add files via upload

---
+++ co6/q3_co6/q3_co6.java
@@ -0,0 +1,26 @@
+package study;
+import java.util.*;
+import java.io.*;
+import java.io.File;
+public class q3_co6 {
+	public static void main(String args[]) throws IOException
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the first file:");
+		String file1=sc.next();
+		System.out.println(file1);
+		System.out.println("enter the Second file:");
+		String file2 =sc.next();
+		sc.close();
+		FileReader fin = new FileReader(file1);
+		FileWriter fout = new FileWriter(file2, true);
+		int c;
+		while ((c = fin.read()) != -1){
+			fout.write(c);
+		}
+		System.out.println("copy file1 to file2 ");
+		fin.close();
+		fout.close();
+	}
+}
+
