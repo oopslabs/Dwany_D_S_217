7ac7b58072d2414b5aac578047f5ab12bae6e33b
dwanyds <95312677+dwanyds@users.noreply.github.com>
2022-07-14 08:03:51 +0530

    Update and rename q1 to q1.java

---
+++ co6/q1_co6/q1.java
@@ -0,0 +1,30 @@
+
+package study;
+
+import java.io.*;
+import java.util.*;
+public class co6_q1{
+	public static void main(String[] args) {
+		File file = new File("C:\\Users\\dwany\\OneDrive\\Pictures");
+		String[] list = file.list();
+		for(String str : list){
+			System.out.println(str);
+		}
+		System.out.println("\nSEARCHING FOR FILENAMES STARTING WITH 's':\n");
+		FilenameFilter filter = new FilenameFilter() {
+			public boolean accept(File dir, String fname) {
+				return fname.startsWith("S");
+			}
+		}; 
+		String[] search =file.list(filter);
+		if(search.length==0){
+			System.out.println("File does not exist.");
+		}
+		else {
+			for(int i=0; i<search.length;i++) {
+				String fn = search[i];
+				System.out.println(fn);
+			}
+		}
+	}
+}
